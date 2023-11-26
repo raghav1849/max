94ef5bd57e01e5d2540678d0612ad9538091c174
Raghav Saini <raghavsaini30795@gmail.com>
2023-11-26 22:51:59 +0530

    setup cloudinary & multer

--- server/src/models/supplier.model.js
+++ server/src/models/supplier.model.js
@@ -3,78 +3,77 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
 const supplierSchema = new Schema(
-  {
-    supplierId: {
-      type: String,
-      required: true,
-      index: true,
-      unique: true,
+    {
+        supplierId: {
+            type: String,
+            required: true,
+            index: true,
+            unique: true,
+        },
+        businessName: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        firstName: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        lastName: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            trim: true,
+            lowercase: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        avatar: {
+            type: String, // Cloudinary url
+            required: true,
+        },
     },
-    businessName: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    firstName: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    lastName: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      trim: true,
-      lowercase: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    avatar: {
-      type: String, // Cloudinary url
-      required: true,
-    },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
 supplierSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
 });
 
 supplierSchema.methods.isPasswordCorrect = async function (password) {
-  return await bcrypt.compare(password, this.password);
+    return await bcrypt.compare(password, this.password);
 };
 
 supplierSchema.methods.generateAccessToken = async function () {
-  return jwt.sign(
-    {
-      _id: this._id,
-      suppliedId: this.ssupplierId,
-      email: this.email,
-      businessName: this.businessName,
-    },
-    process.env.ACCESS_TOKEN_SECRET,
-    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
-  );
+    return jwt.sign(
+        {
+            _id: this._id,
+            suppliedId: this.ssupplierId,
+            email: this.email,
+            businessName: this.businessName,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+    );
 };
 
-
 supplierSchema.methods.generateRefreshToken = async function () {
-  return jwt.sign(
-    {
-      _id: this._id,
-    },
-    process.env.REFRESH_TOKEN_SECRET,
-    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
-  );
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+    );
 };
 
 const Supplier = mongoose.model("Supplier", supplierSchema);
--- server/src/models/supplierAddress.model.js
+++ server/src/models/supplierAddress.model.js
@@ -1,33 +1,33 @@
 import mongoose, { Schema } from "mongoose";
 
 const supplierAddressSchema = new Schema({
-  supplierId: {
-    type: Schema.Types.String,
-    ref: "Supplier",
-    required: true,
-    index: true,
-  },
-  apartment: {
-    type: String,
-    required: true,
-  },
-  streetAddress: {
-    type: String,
-    required: true,
-  },
-  city: {
-    type: String,
-    required: true,
-  },
-  country: {
-    type: String,
-    required: true,
-  },
+    supplierId: {
+        type: Schema.Types.String,
+        ref: "Supplier",
+        required: true,
+        index: true,
+    },
+    apartment: {
+        type: String,
+        required: true,
+    },
+    streetAddress: {
+        type: String,
+        required: true,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    country: {
+        type: String,
+        required: true,
+    },
 });
 
 const SupplierAddress = mongoose.model(
-  "SupplierAddress",
-  supplierAddressSchema
+    "SupplierAddress",
+    supplierAddressSchema
 );
 
 export default SupplierAddress;
